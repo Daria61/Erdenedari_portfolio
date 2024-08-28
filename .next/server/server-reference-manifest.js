d7a05a43a9028f5f74d45464a9a95df5896a4705
Erdenedari <107394167+Daria61@users.noreply.github.com>
2024-08-29 01:51:37 +0800

    final project

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"W4Uo4oKdl6BtxSNautLp81v6h7JowQvIbIsuxBrE4kw=\"\n}"
--- src/components/Scrolling.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactLenis, useLenis } from "lenis/react";
-
-export default function Layout() {
-  const lenis = useLenis(({ scroll }) => {
-    // called every scroll
-  });
-
-  return <ReactLenis root>{/* content */}</ReactLenis>;
-}
