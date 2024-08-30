10b6e465afdb5089aed7307fa74e4c0d446ff405
Erdenedari <107394167+Daria61@users.noreply.github.com>
2024-08-30 22:08:55 +0800

    image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         />
         <meta
           property="og:image"
-          content="/public/Screenshot 2024-08-30 at 21.31.55.png"
+          content="https://private-user-images.githubusercontent.com/107394167/363201829-e1a22fdc-caf3-439d-9e51-956f436145dd.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjUwMjY4MjcsIm5iZiI6MTcyNTAyNjUyNywicGF0aCI6Ii8xMDczOTQxNjcvMzYzMjAxODI5LWUxYTIyZmRjLWNhZjMtNDM5ZC05ZTUxLTk1NmY0MzYxNDVkZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwODMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDgzMFQxNDAyMDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iYTc4OGIzNTlkMDc1ZDQzZTZiMzYzNjgzYzUyNDFlNWQ4NDgyYmEzMzkwZjA1OTk3Njc2YzUxNzgxNWMzMjM0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.pz0WmSDbu0qG0nKMevqbado81esqxd5BaAkujJzOdfQ"
         />
         <meta name="twitter:card" content="summary" />
       </head>
